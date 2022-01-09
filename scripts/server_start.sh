cdead30727a60eb8426120c3985b12e0e1385a19
biloocabba <80125020+biloocabba@users.noreply.github.com>
2022-01-09 15:48:09 +0200

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cd /home/ec2-user/KnCare-Rest/target
-sudo nohup java -jar /home/ec2-user/KnCare-0.0.1-SNAPSHOT.jar > log.log 2>&1 &
+cd /home/ec2-user/KnCare-Rest
+sudo nohup java -jar /home/ec2-user/KnCare-Rest/KnCare-0.0.1-SNAPSHOT.jar > log.log 2>&1 &
