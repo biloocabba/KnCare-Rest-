3f281e50c6ce85bb42c042b5dadd14cec90a033a
biloocabba <80125020+biloocabba@users.noreply.github.com>
2022-01-12 14:59:15 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ return this
 def buildImage() {
     echo "building the application..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER' )]) {
-        sh 'docker build -t biloocabba/kncare-app:2.0 . '
+        sh 'docker build -t biloocabba/kncare-app:3.0 . '
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push biloocabba/kncare-app:2.0 '
     }
